21dd43c84b4f2e6f5381ae440196f11058afbe06
jinu220 <133849027+jinucho@users.noreply.github.com>
2024-01-09 14:15:04 +0900

    Update sample_code.py

--- Mean_30/Streamlit_Service/sample_code.py
+++ Mean_30/Streamlit_Service/sample_code.py
@@ -6,8 +6,219 @@ warnings.filterwarnings(action='ignore')
 from pycaret.classification import *
 import pickle
 import streamlit as st
-import requests
 
+# about_info = """
+# NHIS2018 데이터 기반 당뇨 예측 모델
+# """
+# st.sidebar.title("About")
+# st.sidebar.info(about_info)
+
+# logo_url = "https://www.biotimes.co.kr/news/photo/202204/7608_8719_340.jpg"
+# st.sidebar.image(logo_url)
+
+# # ref_df = pd.read_csv('설문지_CV.csv',index_col = 'Unnamed: 0')
+# url = 'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/ref_DataFrame.csv'
+# ref_df = pd.read_csv(url,index_col = 'Unnamed: 0')
+# pkl_list = ['ensemble_model.pickle', 'Scaler_AGE.pickle', 'Scaler_BMI.pickle', 'Scaler_HEIGHT(cm).pickle', 'Scaler_WEIGHT(kg).pickle' ]
+# val_list = ['model', 'AGE', 'BMI', 'HEIGHT(cm)', 'WEIGHT(kg)']
+# scalers = ['AGE', 'BMI', 'HEIGHT(cm)', 'WEIGHT(kg)']
+
+# # Load 피클 데이터 
+# list_dic = {}
+# val_list = ['model', 'AGE', 'BMI', 'HEIGHT(cm)', 'WEIGHT(kg)']
+# urls = [
+#     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/ensemble_model.pickle',
+#     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/Scaler_AGE.pickle',
+#     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/Scaler_BMI.pickle',
+#     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/Scaler_HEIGHT(cm).pickle',
+#     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/Scaler_WEIGHT(kg).pickle'
+# ]
+# for val,url in zip(val_list,urls):
+#     response = requests.get(url)
+#     data = response.content
+#     list_dic[val] = pickle.loads(data)
+
+
+# def validate_zero_one(text):
+#     return text if text in ['0', '1'] else None
+
+# def answerconvert(x):
+#     if x == '예' or x =='여성':
+#         return 1
+#     else : 
+#         return 0
+
+
+# st.title('당신은 당뇨 위험이 있을까?')
+
+# feature1 = st.text_input('이름을 입력하세요:', ) #-> ID로 사용
+# feature2 = st.text_input('나이를 입력하세요:', )
+
+
+
+# feature18 = st.text_input('키를 입력하세요:', )
+# feature19 = st.text_input('몸무게를 입력하세요:', )
+
+
+
+
+# feature3 = st.radio('음주를 자주 하나요?:',('예','아니오'))
+# feature3 = answerconvert(feature3)
+
+
+# feature4 = st.radio('관절염이 있나요:',('예','아니오'))
+# feature4 = answerconvert(feature4)
+    
+
+# feature6 = st.radio('콜레스테롤이 높나요?:',('예','아니오'))
+# feature6 = answerconvert(feature6)
+
+# feature7 = st.radio('폐기종이 있나요?:',('예','아니오'))
+# feature7 = answerconvert(feature7)
+
+
+# feature8 = st.radio('균형잡힌 식사를 하나요?:',('예','아니오'))
+# feature8 = answerconvert(feature8)
+
+# feature9 = st.radio('성별을 선택하세요:',('남성','여성'))
+# feature9 = answerconvert(feature9)
+
+# feature11 = st.radio('고혈압이 있나요?:',('예','아니오'))
+# feature11 = answerconvert(feature11)
+
+# feature12 = st.radio('이전에 혈압약을 먹었나요?:',('예','아니오'))
+# feature12 = answerconvert(feature12)
+
+# feature13 = st.radio('현재 혈압약 먹나요?:',('예','아니오'))
+# feature13 = answerconvert(feature13)
+
+# feature14 = st.radio('최근 복통/구토/설사등 위장 장애가 있었나요?:',('예','아니오'))
+# feature14 = answerconvert(feature14)
+    
+# feature10 = st.radio('히스패닉인가요?:',('예','아니오'))
+# feature10 = answerconvert(feature10)
+
+    
+# race = {"백인": 1,"흑인": 2,"인도인": 3,"중국인":6,"필리핀":7,"아시아인":12,"혼혈":17,"이외":16}
+# selected_race = st.selectbox('인종을 선택해주세요:', list(race.keys()))
+# feature15 = race[selected_race]
+# feature16 = feature15
+# # region = {"동북": 1,"중서부": 2,"남부": 3,"서부":4}
+# # selected_region = st.selectbox('지역을 선택해주세요:', list(region.keys()))
+# # feature17 = region[selected_region]
+# feature17 = 1
+
+# train_col = ['AGE', 'ALCSTAT', 'ARTH1', 'BMI', 'CHLEV', 'EPHEV', 'FSBALANC',
+#        'GENDER', 'HYPEV', 'HYPMDEV2', 'HYPMED2', 'INTIL2W', 'HEIGHT(cm)',
+#        'WEIGHT(kg)', 'HISPAN_I_0.0', 'HISPAN_I_1.0', 'HISPAN_I_2.0',
+#        'HISPAN_I_3.0', 'HISPAN_I_4.0', 'HISPAN_I_5.0', 'HISPAN_I_6.0',
+#        'HISPAN_I_7.0', 'HISPAN_I_8.0', 'HISPAN_I_12.0', 'MRACBPI2_1.0',
+#        'MRACBPI2_2.0', 'MRACBPI2_3.0', 'MRACBPI2_6.0', 'MRACBPI2_7.0',
+#        'MRACBPI2_12.0', 'MRACBPI2_16.0', 'MRACBPI2_17.0', 'MRACRPI2_1.0',
+#        'MRACRPI2_2.0', 'MRACRPI2_3.0', 'MRACRPI2_9.0', 'MRACRPI2_10.0',
+#        'MRACRPI2_11.0', 'MRACRPI2_15.0', 'MRACRPI2_16.0', 'MRACRPI2_17.0',
+#        'REGION_1.0', 'REGION_2.0', 'REGION_3.0', 'REGION_4.0']
+# if 'input_data' not in st.session_state:
+#     st.session_state['input_data'] = None
+# if 'survey_result' not in st.session_state:
+#     st.session_state['survey_result'] = None
+
+# if st.button('설문 결과'):
+#     try:
+#         feature5 = str(int(int(feature19)/((int(feature18)/100)**2)))
+#         col_text = 'id AGE ALCSTAT ARTH1 BMI CHLEV EPHEV FSBALANC GENDER HISPAN_I HYPEV HYPMDEV2 HYPMED2 INTIL2W MRACBPI2 MRACRPI2 REGION HEIGHT(cm) WEIGHT(kg) DIBEV1'
+#         columns = col_text.split(' ')
+#         user_input_feature = [feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9,feature10,feature11,feature12,feature13,feature14,feature15,feature16,feature17,feature18,feature19]
+#         data = dict(zip(columns,user_input_feature))
+#         input_df = pd.DataFrame(data,index=[0])
+#         st.session_state['input_data'] = input_df
+#         st.session_state['survey_result'] = input_df
+#         # st.write('설문결과')
+#         # st.dataframe(input_df)
+#         if st.session_state['survey_result'] is not None:
+#             st.write('이전 설문 결과:')
+#             st.dataframe(st.session_state['survey_result'])
+#     except:
+#         st.write('누락된 값이 있습니다.')
+        
+
+
+# if st.button('당신은 당뇨일까?!'):
+#     if st.session_state['input_data'] is not None:
+#         # session_state에서 데이터 불러오기
+#         input_df = st.session_state['input_data']
+
+#         try:
+#             num_col = ['AGE','BMI','HEIGHT(cm)','WEIGHT(kg)']
+#             cat_col = ['HISPAN_I', 'MRACBPI2', 'MRACRPI2', 'REGION']
+#             nom_col = np.setdiff1d(input_df.drop('id',axis=1).columns,num_col,cat_col)
+#             input_df.loc[:,num_col] = input_df.loc[:,num_col].astype('float')
+#             input_df.loc[:,nom_col] = input_df.loc[:,nom_col].astype('int')
+    
+#             cust_id = input_df.pop('id')
+    
+#             # Scaler 생성(from pickle) 할당 및 컬럼 Scaling
+#             for s in scalers:
+#                 scale = list_dic[s]
+#                 input_df[s] = scale.transform(input_df[[s]])
+#             # 명목형 컬럼들에 대한 dummy 데이터 생성(원핫인코딩)
+#             test_dummies = [] # 검증용 데이터셋의 명목형 컬럼들의 더미데이터셋 저장용 리스트
+#             for col in cat_col:
+#                 test_dummies.append(pd.get_dummies(input_df[col],prefix=col,dtype='int')) # 검증데이터의 각 컬럼들의 더미데이터셋을 리스트에 저장
+    
+#             test_dummies = pd.concat(test_dummies,axis=1)    
+#             input_df = pd.concat([input_df,test_dummies],axis=1).drop(cat_col,axis=1)
+    
+    
+#             for col in train_col:
+#                 if col not in input_df.columns:
+#                     input_df[col] = 0
+    
+#             # for col in input_df.columns:
+#             #     if col not in train_col:
+#             #         input_df.drop(col,axis=1)
+    
+#             input_df = input_df[train_col]
+#             # st.write(f'{input_df.columns}')#,{y_prob}')
+#             # 모델 생성(from pickle)
+#             model = list_dic['model']
+    
+#     #     # 당뇨병 예측
+#             y_pred = model.predict(input_df)
+#             y_prob = model.predict_proba(input_df)[:,1]
+#             result = '1'
+#             if y_pred == 0:
+#                 result = '정상'
+#             else : 
+#                 result = '당뇨'
+    
+#             percent = round(y_prob[0],2)*100
+#     #         # y_prob = round(y_prob[0][0]*100,2)   
+#     #         st.write(f'''{feature1}님은 현재 {result} 입니다.
+            
+            
+#     # 또한, 당뇨일 확률은 {percent}% 입니다.''')
+#             st.sidebar.title("결과 : ")
+#             st.sidebar.info(f'''{feature1}님은 현재 {result} 입니다.
+#     또한, 당뇨일 확률은 {percent}% 입니다.''')
+#             st.info(f"결과 : {feature1}님은 현재 {result} 입니다. 또한, 당뇨일 확률은 {percent} % 입니다.")
+    
+#         except:
+#             pass
+
+
+
+
+# GPT코드
+# Streamlit session state 초기화
+if 'input_data' not in st.session_state:
+    st.session_state['input_data'] = None
+if 'survey_result' not in st.session_state:
+    st.session_state['survey_result'] = None
+
+
+
+# 앱의 상단에 대한 정보
 about_info = """
 NHIS2018 데이터 기반 당뇨 예측 모델
 """
@@ -17,192 +228,72 @@ st.sidebar.info(about_info)
 logo_url = "https://www.biotimes.co.kr/news/photo/202204/7608_8719_340.jpg"
 st.sidebar.image(logo_url)
 
-# ref_df = pd.read_csv('설문지_CV.csv',index_col = 'Unnamed: 0')
+# 데이터 로드
 url = 'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/ref_DataFrame.csv'
-ref_df = pd.read_csv(url,index_col = 'Unnamed: 0')
-pkl_list = ['ensemble_model.pickle', 'Scaler_AGE.pickle', 'Scaler_BMI.pickle', 'Scaler_HEIGHT(cm).pickle', 'Scaler_WEIGHT(kg).pickle' ]
-val_list = ['model', 'AGE', 'BMI', 'HEIGHT(cm)', 'WEIGHT(kg)']
-scalers = ['AGE', 'BMI', 'HEIGHT(cm)', 'WEIGHT(kg)']
+ref_df = pd.read_csv(url, index_col='Unnamed: 0')
 
-# Load 피클 데이터 
-list_dic = {}
-val_list = ['model', 'AGE', 'BMI', 'HEIGHT(cm)', 'WEIGHT(kg)']
-urls = [
+# 모델 및 스케일러 로드
+pkl_urls = [
     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/ensemble_model.pickle',
     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/Scaler_AGE.pickle',
     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/Scaler_BMI.pickle',
     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/Scaler_HEIGHT(cm).pickle',
     'https://raw.githubusercontent.com/jinucho/Mean_30/main/Streamlit_Service/pkl/Scaler_WEIGHT(kg).pickle'
 ]
-for val,url in zip(val_list,urls):
-    response = requests.get(url)
-    data = response.content
-    list_dic[val] = pickle.loads(data)
+val_list = ['model', 'AGE', 'BMI', 'HEIGHT(cm)', 'WEIGHT(kg)']
+list_dic = {val: pickle.loads(requests.get(url).content) for val, url in zip(val_list, pkl_urls)}
 
+# 사용자 입력 처리 함수
+def process_user_input():
+    try:
+        # 사용자 입력 처리 로직
+        # ...
+        # 예시: 사용자 입력을 DataFrame 형태로 변환하는 로직
+        globals()['input_df'] = pd.DataFrame(data,index=[0])
+    except Exception as e:
+        st.error(f"입력 처리 중 오류가 발생했습니다: {e}")
 
-def validate_zero_one(text):
-    return text if text in ['0', '1'] else None
-
-def answerconvert(x):
-    if x == '예' or x =='여성':
-        return 1
-    else : 
-        return 0
-
-
+# 사용자 입력 폼
 st.title('당신은 당뇨 위험이 있을까?')
 
-feature1 = st.text_input('이름을 입력하세요:', ) #-> ID로 사용
+# 사용자 입력 관련 코드
+# 예시: 사용자로부터 입력을 받는 필드
+feature1 = st.text_input('이름을 입력하세요:', )
 feature2 = st.text_input('나이를 입력하세요:', )
+# ...
+# 나머지 필드 추가
+# ...
 
-
-
-feature18 = st.text_input('키를 입력하세요:', )
-feature19 = st.text_input('몸무게를 입력하세요:', )
-
-
-
-
-feature3 = st.radio('음주를 자주 하나요?:',('예','아니오'))
-feature3 = answerconvert(feature3)
-
-
-feature4 = st.radio('관절염이 있나요:',('예','아니오'))
-feature4 = answerconvert(feature4)
-    
-
-feature6 = st.radio('콜레스테롤이 높나요?:',('예','아니오'))
-feature6 = answerconvert(feature6)
-
-feature7 = st.radio('폐기종이 있나요?:',('예','아니오'))
-feature7 = answerconvert(feature7)
-
-
-feature8 = st.radio('균형잡힌 식사를 하나요?:',('예','아니오'))
-feature8 = answerconvert(feature8)
-
-feature9 = st.radio('성별을 선택하세요:',('남성','여성'))
-feature9 = answerconvert(feature9)
-
-feature11 = st.radio('고혈압이 있나요?:',('예','아니오'))
-feature11 = answerconvert(feature11)
-
-feature12 = st.radio('이전에 혈압약을 먹었나요?:',('예','아니오'))
-feature12 = answerconvert(feature12)
-
-feature13 = st.radio('현재 혈압약 먹나요?:',('예','아니오'))
-feature13 = answerconvert(feature13)
-
-feature14 = st.radio('최근 복통/구토/설사등 위장 장애가 있었나요?:',('예','아니오'))
-feature14 = answerconvert(feature14)
-    
-feature10 = st.radio('히스패닉인가요?:',('예','아니오'))
-feature10 = answerconvert(feature10)
-
-    
-race = {"백인": 1,"흑인": 2,"인도인": 3,"중국인":6,"필리핀":7,"아시아인":12,"혼혈":17,"이외":16}
-selected_race = st.selectbox('인종을 선택해주세요:', list(race.keys()))
-feature15 = race[selected_race]
-feature16 = feature15
-# region = {"동북": 1,"중서부": 2,"남부": 3,"서부":4}
-# selected_region = st.selectbox('지역을 선택해주세요:', list(region.keys()))
-# feature17 = region[selected_region]
-feature17 = 1
-
-train_col = ['AGE', 'ALCSTAT', 'ARTH1', 'BMI', 'CHLEV', 'EPHEV', 'FSBALANC',
-       'GENDER', 'HYPEV', 'HYPMDEV2', 'HYPMED2', 'INTIL2W', 'HEIGHT(cm)',
-       'WEIGHT(kg)', 'HISPAN_I_0.0', 'HISPAN_I_1.0', 'HISPAN_I_2.0',
-       'HISPAN_I_3.0', 'HISPAN_I_4.0', 'HISPAN_I_5.0', 'HISPAN_I_6.0',
-       'HISPAN_I_7.0', 'HISPAN_I_8.0', 'HISPAN_I_12.0', 'MRACBPI2_1.0',
-       'MRACBPI2_2.0', 'MRACBPI2_3.0', 'MRACBPI2_6.0', 'MRACBPI2_7.0',
-       'MRACBPI2_12.0', 'MRACBPI2_16.0', 'MRACBPI2_17.0', 'MRACRPI2_1.0',
-       'MRACRPI2_2.0', 'MRACRPI2_3.0', 'MRACRPI2_9.0', 'MRACRPI2_10.0',
-       'MRACRPI2_11.0', 'MRACRPI2_15.0', 'MRACRPI2_16.0', 'MRACRPI2_17.0',
-       'REGION_1.0', 'REGION_2.0', 'REGION_3.0', 'REGION_4.0']
-if 'input_data' not in st.session_state:
-    st.session_state['input_data'] = None
-if 'survey_result' not in st.session_state:
-    st.session_state['survey_result'] = None
-
+# 설문 결과 처리
 if st.button('설문 결과'):
-    try:
-        feature5 = str(int(int(feature19)/((int(feature18)/100)**2)))
-        col_text = 'id AGE ALCSTAT ARTH1 BMI CHLEV EPHEV FSBALANC GENDER HISPAN_I HYPEV HYPMDEV2 HYPMED2 INTIL2W MRACBPI2 MRACRPI2 REGION HEIGHT(cm) WEIGHT(kg) DIBEV1'
-        columns = col_text.split(' ')
-        user_input_feature = [feature1,feature2,feature3,feature4,feature5,feature6,feature7,feature8,feature9,feature10,feature11,feature12,feature13,feature14,feature15,feature16,feature17,feature18,feature19]
-        data = dict(zip(columns,user_input_feature))
-        input_df = pd.DataFrame(data,index=[0])
-        st.session_state['input_data'] = input_df
-        st.session_state['survey_result'] = input_df
-        # st.write('설문결과')
-        # st.dataframe(input_df)
-        if st.session_state['survey_result'] is not None:
-            st.write('이전 설문 결과:')
-            st.dataframe(st.session_state['survey_result'])
-    except:
-        st.write('누락된 값이 있습니다.')
-        
+    process_user_input()
+    st.session_state['input_data'] = input_df
+    st.session_state['survey_result'] = input_df
+    st.write('설문결과')
+    st.dataframe(input_df)
 
-
+# 당뇨병 예측 처리
 if st.button('당신은 당뇨일까?!'):
     if st.session_state['input_data'] is not None:
-        # session_state에서 데이터 불러오기
         input_df = st.session_state['input_data']
+        # 당뇨병 예측 로직
+        # 예시: 당뇨병 예측 모델을 사용하여 예측 수행
+        model = list_dic['model']
+        y_pred = model.predict(input_df)
+        y_prob = model.predict_proba(input_df)[:,1]
+        # ...
+        # 예측 결과 표시
+        st.write(f"예측 결과: {y_pred}")
+        st.write(f"당뇨병 확률: {y_prob}")
+    else:
+        st.warning('먼저 설문 결과를 입력해주세요!')
 
-        try:
-            num_col = ['AGE','BMI','HEIGHT(cm)','WEIGHT(kg)']
-            cat_col = ['HISPAN_I', 'MRACBPI2', 'MRACRPI2', 'REGION']
-            nom_col = np.setdiff1d(input_df.drop('id',axis=1).columns,num_col,cat_col)
-            input_df.loc[:,num_col] = input_df.loc[:,num_col].astype('float')
-            input_df.loc[:,nom_col] = input_df.loc[:,nom_col].astype('int')
-    
-            cust_id = input_df.pop('id')
-    
-            # Scaler 생성(from pickle) 할당 및 컬럼 Scaling
-            for s in scalers:
-                scale = list_dic[s]
-                input_df[s] = scale.transform(input_df[[s]])
-            # 명목형 컬럼들에 대한 dummy 데이터 생성(원핫인코딩)
-            test_dummies = [] # 검증용 데이터셋의 명목형 컬럼들의 더미데이터셋 저장용 리스트
-            for col in cat_col:
-                test_dummies.append(pd.get_dummies(input_df[col],prefix=col,dtype='int')) # 검증데이터의 각 컬럼들의 더미데이터셋을 리스트에 저장
-    
-            test_dummies = pd.concat(test_dummies,axis=1)    
-            input_df = pd.concat([input_df,test_dummies],axis=1).drop(cat_col,axis=1)
-    
-    
-            for col in train_col:
-                if col not in input_df.columns:
-                    input_df[col] = 0
-    
-            # for col in input_df.columns:
-            #     if col not in train_col:
-            #         input_df.drop(col,axis=1)
-    
-            input_df = input_df[train_col]
-            # st.write(f'{input_df.columns}')#,{y_prob}')
-            # 모델 생성(from pickle)
-            model = list_dic['model']
-    
-    #     # 당뇨병 예측
-            y_pred = model.predict(input_df)
-            y_prob = model.predict_proba(input_df)[:,1]
-            result = '1'
-            if y_pred == 0:
-                result = '정상'
-            else : 
-                result = '당뇨'
-    
-            percent = round(y_prob[0],2)*100
-    #         # y_prob = round(y_prob[0][0]*100,2)   
-    #         st.write(f'''{feature1}님은 현재 {result} 입니다.
-            
-            
-    # 또한, 당뇨일 확률은 {percent}% 입니다.''')
-            st.sidebar.title("결과 : ")
-            st.sidebar.info(f'''{feature1}님은 현재 {result} 입니다.
-    또한, 당뇨일 확률은 {percent}% 입니다.''')
-            st.info(f"결과 : {feature1}님은 현재 {result} 입니다. 또한, 당뇨일 확률은 {percent} % 입니다.")
-    
-        except:
-            pass
+# 저장된 설문 결과를 표시합니다.
+if st.session_state['survey_result'] is not None:
+    st.write('이전 설문 결과:')
+    st.dataframe(st.session_state['survey_result'])
+
+# 여기에 나머지 코드를 계속해서 추가합니다.
+# ...
+
+
